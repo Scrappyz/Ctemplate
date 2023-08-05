24770095adb50a498213534144b64a1e819fcc09
Scrappyz <ghostmic3000@gmail.com>
2023-08-05 12:06:14 +0800

    Troubleshooting

--- test/src/project_template_test.cpp
+++ test/src/project_template_test.cpp
@@ -4,8 +4,14 @@
 
 std::string test_path = path::joinPath(path::sourcePath(), "../test_path");
 std::string config_path = path::joinPath(test_path, "program/config.txt");
+std::string template_dir = path::joinPath(test_path, "templates");
+std::string destination = path::joinPath(test_path, "temp");
 
-TEST(some_test, some_test)
+TEST(initTemplate, initialized)
 {
-    
+    CLI cli({"project_template_test.exe", "init", "t1", "-p", destination});
+    Config config(config_path);
+    //initTemplate(cli, config); // BUG: this makes the unit test binary disappear
+    //ASSERT_TRUE(path::hasSameContent(path::joinPath(template_dir, "t1"), destination));
+    //path::remove(destination + '/');
 }
