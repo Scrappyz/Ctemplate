602ac319caf6efc1c299a5c2ed275808a66c6cf3
Scrappyz <ghostmic3000@gmail.com>
2024-05-26 12:07:15 +0800

    removed unneeded code

--- include/util.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "config.hpp"
-
-std::string getSpaces(int spaces, const std::string& str);
-std::string trim(const std::string& str);
-std::string getTemplateDirectory(const Config& config);
-std::string getConfigValue(const Config& config, const std::string& section, const std::string& key);
-std::string getConfigValue(const Config& config, const std::string& key);
