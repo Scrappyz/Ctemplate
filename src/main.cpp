e13476d18cbcfe900b9232709e55b7148682a4fd
Scrappyz <ghostmic3000@gmail.com>
2024-06-03 14:45:23 +0800

    added options

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,57 @@
 #include <iostream>
-#include "cxxopts.hpp"
+#include "CLI11.hpp"
+
+void listTemplates()
+{
+    std::cout << "list templates" << std::endl;
+}
 
 int main(int argc, char** argv)
 {
-    cxxopts::Options options("ctemplate", "Create custom boilerplate code");
+    CLI::App app("Ctemplate");
+    app.set_version_flag("-v,--version", "v1.0.0");
 
-    options.add_options()
-        ("b,bar", "Param bar", cxxopts::value<std::string>())
-        ("d,debug", "Enable debugging", cxxopts::value<bool>()->default_value("false"))
-        ("f,foo", "Param foo", cxxopts::value<int>()->default_value("10"))
-        ("h,help", "Print usage");
+    bool list_template = false;
 
-    auto result = options.parse(argc, argv);
+    CLI::App* init = app.add_subcommand("init", "Initialize a template");
+    init->add_flag("-l,--list", list_template, "List existing templates");
+    std::string init_template_name;
+    init->add_option("name", init_template_name, "Name of the template to initialize");
+    
+    CLI::App* add = app.add_subcommand("add", "Add a new template");
+    std::string add_path;
+    add->add_option("path", add_path, "Root project directory of template to be added")->expected(1);
+    std::string add_template_name;
+    add->add_option("-n,--name", add_template_name, "Name of new template")->expected(1);
+    std::string add_template_desc;
+    add->add_option("-d,--desc", add_template_desc, "Description of new template")->expected(1);
 
-    if (result.count("help"))
-    {
-      std::cout << options.help() << std::endl;
-      exit(0);
+    CLI::App* remove = app.add_subcommand("remove", "Remove an existing template");
+    std::vector<std::string> remove_template_names;
+    remove->add_option("names", remove_template_names, "Name of template to be removed");
+
+    CLI::App* list = app.add_subcommand("list", "List all templates");
+
+    CLI11_PARSE(app, argc, argv);
+
+    if(*init) {
+        std::cout << "init" << std::endl;
+        if(list_template) {
+            listTemplates();
+        }
+    } else if(*add) {
+        std::cout << add_path << std::endl;
+        std::cout << add_template_name << std::endl;
+        std::cout << add_template_desc << std::endl;
+    } else if(*remove) {
+        for(const auto& i : remove_template_names) {
+            std::cout << i << std::endl;
+        }
+    } else if(*list) {
+        listTemplates();
+    } else {
+        std::cout << "nuh uh" << std::endl;
     }
-    bool debug = result["debug"].as<bool>();
-    std::string bar;
-    if (result.count("bar"))
-      bar = result["bar"].as<std::string>();
-    int foo = result["foo"].as<int>();
 
     return 0;
 }
