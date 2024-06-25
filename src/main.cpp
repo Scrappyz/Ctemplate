039cb61df970b69058e25f4dac0a568f960a6d74
Scrappyz <ghostmic3000@gmail.com>
2024-06-25 21:00:08 +0800

    added addTemplate

--- src/main.cpp
+++ src/main.cpp
@@ -47,21 +47,20 @@ void addTemplate(const std::string& template_dir, const std::string& path_to_add
         path::createDirectory(new_template_path);
     }
 
-    path::copy(path_to_add, new_template_path, path::CopyOption::None);
+    // Add directory separator to only copy the contents of the path and not its folder
+    path::copy(path_to_add + path::directorySeparator(), new_template_path, path::CopyOption::None);
 
-    if(!path::exists(container_name)) {
-        path::createDirectory(container_name);
+    std::string new_container_path = path::joinPath(new_template_path, container_name);
+
+    if(path::exists(new_container_path)) {
+        path::remove(new_container_path);
     }
+
+    path::createDirectory(new_container_path);
 
     json info = {
         {"description", desc}
     };
-    json paths = json::parse(R"(
-        {
-            "includePaths": [],
-            "excludePaths": []
-        }
-    )");
     json variables = json::parse(R"(
         {
             "includePaths": [],
@@ -72,10 +71,8 @@ void addTemplate(const std::string& template_dir, const std::string& path_to_add
         }
     )");
 
-    std::string container_path = path::joinPath(new_template_path, container_name);
-    writeJsonToFile(info, path::joinPath(container_path, "info.json"), 4);
-    writeJsonToFile(paths, path::joinPath(container_path, "paths.json"), 4);
-    writeJsonToFile(variables, path::joinPath(container_path, "variables.json"), 4);
+    writeJsonToFile(info, path::joinPath(new_container_path, "info.json"), 4);
+    writeJsonToFile(variables, path::joinPath(new_container_path, "variables.json"), 4);
 }
 
 void removeTemplates(const std::string& template_dir, const std::vector<std::string>& templates)
@@ -142,99 +139,94 @@ int main(int argc, char** argv)
     CLI::App app("Ctemplate");
     app.set_version_flag("-v,--version", "v1.0.0");
 
-    // // Default settings
-    // json app_config = {
-    //     {"templateDirectory", path::joinPath(path::sourcePath(), "templates")},
-    //     {"containerName", ".ctemplate"}
-    // };
+    // Default settings
+    json app_config = {
+        {"templateDirectory", path::joinPath(path::sourcePath(), "templates")},
+        {"containerName", ".ctemplate"}
+    };
 
-    // std::string config_file_path = path::joinPath(path::sourcePath(), "config.json");
+    std::string config_file_path = path::joinPath(path::sourcePath(), "config.json");
 
-    // // if config file exists, read from it. Else, use default settings then create the config file.
-    // if(path::exists(config_file_path)) {
-    //     app_config = readJsonFromFile(config_file_path);
-    // } else {
-    //     writeJsonToFile(app_config, config_file_path, 4);
-    // }
+    // if config file exists, read from it. Else, use default settings then create the config file.
+    if(path::exists(config_file_path)) {
+        app_config = readJsonFromFile(config_file_path);
+    } else {
+        writeJsonToFile(app_config, config_file_path, 4);
+    }
 
-    // // Directory where templates are stored
-    // std::string template_dir = app_config.at("templateDirectory");
+    // Directory where templates are stored
+    std::string template_dir = app_config.at("templateDirectory");
 
-    // // Check if absolute path or relative path. If relative, make it absolute, relative to source
-    // if(!path::isAbsolutePath(template_dir)) {
-    //     template_dir = path::joinPath(path::sourcePath(), template_dir);
-    // }
+    // Check if absolute path or relative path. If relative, make it absolute, relative to source
+    if(!path::isAbsolutePath(template_dir)) {
+        template_dir = path::joinPath(path::sourcePath(), template_dir);
+    }
 
-    // // If template directory does not exist, create one
-    // if(!path::exists(template_dir)) {
-    //     path::createDirectory(template_dir);
-    // }
+    // If template directory does not exist, create one
+    if(!path::exists(template_dir)) {
+        path::createDirectory(template_dir);
+    }
 
-    // std::string container_name = app_config.at("containerName");
+    std::string container_name = app_config.at("containerName");
 
-    // bool list_template = false;
+    bool list_template = false;
 
-    // // For "init" subcommand
-    // CLI::App* init = app.add_subcommand("init", "Initialize a template");
-    // init->add_flag("-l,--list", list_template, "List existing templates");
-    // std::string init_template_name;
-    // init->add_option("name", init_template_name, "Name of the template to initialize");
+    // For "init" subcommand
+    CLI::App* init = app.add_subcommand("init", "Initialize a template");
+    init->add_flag("-l,--list", list_template, "List existing templates");
+    std::string init_template_name;
+    init->add_option("name", init_template_name, "Name of the template to initialize");
     
-    // // For "add" subcommand
-    // CLI::App* add = app.add_subcommand("add", "Add a new template");
-    // std::string add_path;
-    // add->add_option("path", add_path, "Root project directory of template to be added")->expected(1);
-    // std::string add_template_name;
-    // add->add_option("-n,--name", add_template_name, "Name of new template")->expected(1)->required();
-    // std::string add_template_desc;
-    // add->add_option("-d,--desc", add_template_desc, "Description of new template")->expected(1);
+    // For "add" subcommand
+    CLI::App* add = app.add_subcommand("add", "Add a new template");
+    std::string add_path;
+    add->add_option("path", add_path, "Root project directory of template to be added")->expected(1);
+    std::string add_template_name;
+    add->add_option("-n,--name", add_template_name, "Name of new template")->expected(1)->required();
+    std::string add_template_desc;
+    add->add_option("-d,--desc", add_template_desc, "Description of new template")->expected(1);
 
-    // // For "remove" subcommand
-    // CLI::App* remove = app.add_subcommand("remove", "Remove an existing template");
-    // std::vector<std::string> remove_template_names;
-    // remove->add_option("names", remove_template_names, "Name of template to be removed");
+    // For "remove" subcommand
+    CLI::App* remove = app.add_subcommand("remove", "Remove an existing template");
+    std::vector<std::string> remove_template_names;
+    remove->add_option("names", remove_template_names, "Name of template to be removed");
 
-    // // For "list" subcommand
-    // CLI::App* list = app.add_subcommand("list", "List all templates");
+    // For "list" subcommand
+    CLI::App* list = app.add_subcommand("list", "List all templates");
 
-    // // For "config" subcommand
-    // CLI::App* config = app.add_subcommand("config", "Show config");
+    // For "config" subcommand
+    CLI::App* config = app.add_subcommand("config", "Show config");
 
-    // // For "config set" subcommand
-    // CLI::App* set = config->add_subcommand("set", "Set config values");
-    // std::vector<std::string> config_set_values;
-    // set->add_option("keyvalue", config_set_values, "Config values to change (eg: \"key=value\")");
+    // For "config set" subcommand
+    CLI::App* set = config->add_subcommand("set", "Set config values");
+    std::vector<std::string> config_set_values;
+    set->add_option("keyvalue", config_set_values, "Config values to change (eg: \"key=value\")");
 
-    // CLI11_PARSE(app, argc, argv);
+    CLI11_PARSE(app, argc, argv);
 
-    // if(*init) {
-    //     std::cout << "init" << std::endl;
-    //     if(list_template) {
-    //         listTemplates(template_dir, container_name);
-    //     }
-    // } else if(*add) {
-    //     std::string path_to_add = path::joinPath(path::currentPath(), add_path);
-    //     addTemplate(template_dir, path_to_add, add_template_name, add_template_desc, container_name);
-    // } else if(*remove) {
-    //     removeTemplates(template_dir, remove_template_names);
-    // } else if(*list) {
-    //     listTemplates(template_dir, container_name);
-    // } else if(*config) { // Config commands
-    //     if(*set) { // Change config values
-    //         setConfigValue(app_config, config_set_values);
-    //         writeJsonToFile(app_config, config_file_path, 4);
-    //         return 0;
-    //     }
-    //     std::cout << "Configuration:" << std::endl;
-    //     showConfig(app_config, 2);
-    // } else {
-    //     std::cout << "nuh uh" << std::endl;
-    // }
-
-    std::string file = path::joinPath(path::sourcePath(), "test.txt");
-    std::unordered_map<std::string, std::string> m = {{"str", "motherfucker"}, 
-    {"loop", "while(true) {\n\t}"}};
-    replaceVariablesInFile(file, m, "!", "!");
+    if(*init) {
+        std::cout << "init" << std::endl;
+        if(list_template) {
+            listTemplates(template_dir, container_name);
+        }
+    } else if(*add) {
+        std::string path_to_add = path::joinPath(path::currentPath(), add_path);
+        addTemplate(template_dir, path_to_add, add_template_name, add_template_desc, container_name);
+    } else if(*remove) {
+        removeTemplates(template_dir, remove_template_names);
+    } else if(*list) {
+        listTemplates(template_dir, container_name);
+    } else if(*config) { // Config commands
+        if(*set) { // Change config values
+            setConfigValue(app_config, config_set_values);
+            writeJsonToFile(app_config, config_file_path, 4);
+            return 0;
+        }
+        std::cout << "Configuration:" << std::endl;
+        showConfig(app_config, 2);
+    } else {
+        std::cout << "nuh uh" << std::endl;
+    }
 
     return 0;
 }
